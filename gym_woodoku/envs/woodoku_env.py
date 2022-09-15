ab3649e196f1b2f04a74bef39b4e19d674b080e4
helpingstar <54899900+helpingstar@users.noreply.github.com>
2022-09-15 18:49:25 +0900

    test

--- gym_woodoku/envs/woodoku_env.py
+++ gym_woodoku/envs/woodoku_env.py
@@ -3,16 +3,18 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 
-class SnakeSingleEnv(gym.Env):
+class WoodokuEnv(gym.Env):
 
     def __init__(self):
-        ...
+        pass
 
     def step(self, action):
-        ...
+        reward = action
+        self.state += action
+        return (reward)
 
     def reset(self):
-        ...
+        self.state = 0
 
     def render(self):
-        ...
+        pass
