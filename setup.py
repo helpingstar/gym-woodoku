5592517418102ba0966cfa718934e8590d792669
helpingstar <iamhelpingstar@gmail.com>
2022-11-23 22:19:23 +0900

    fix bug

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name="gym_woodoku",
       version="0.0.1",
       install_requires=[
-          "gym>=0.26.0",
-          "pygame>=2.1.0",
+          "gym>=0.25.0",
+          "pygame==2.1.0",
           "moviepy>=1.0.3",
-          "numpy>=1.22.3"
+          "numpy>=1.18.0"
       ]
       )
