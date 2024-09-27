0b78477eb677fd1181e8952b5dceb80bbc5929a3
helpingstar <iamhelpingstar@gmail.com>
2024-09-28 08:53:03 +0900

    Update pyproject.toml

--- gym_woodoku/__init__.py
+++ gym_woodoku/__init__.py
@@ -1,5 +1,7 @@
 from gymnasium.envs.registration import register
 
+__version__ = "0.0.1"
+
 register(
     id='gym_woodoku/Woodoku-v0',
     entry_point='gym_woodoku.envs:WoodokuEnv',
--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="gym-woodoku",
-    version="0.1.0",
-    author="helpingstar",
-    author_email="iamhelpingstar@gmail.com",
-    description="It is a reinforcement learning environment for block puzzle games.",
-    license="MIT License",
-    packages=find_packages(),
-    install_requires=[
-        "numpy>=1.21.0",
-        "pygame>=2.1.3",
-        "gymnasium>=1.0.0a1",
-        "moviepy>=1.0.0",
-    ],
-    python_requires=">=3.9,<4.0",
-)
+if __name__ == "__main__":
+    setuptools.setup()
